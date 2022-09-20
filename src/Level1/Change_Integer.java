f44bbd0cc638cc5b3ae2465719bdbebe11711cbe
KHS <khs7632@naver.com>
2022-09-20 23:23:00 +0900

    2022.09.20

---
+++ src/Level1/Change_Integer.java
@@ -0,0 +1,19 @@
+package Level1;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+public class Change_Integer {
+
+	public int solution(String s) {
+		int answer = 0;
+		answer = Integer.parseInt(s);		
+		return answer;
+	}//solution() end
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Change_Integer ci = new Change_Integer();
+		//System.out.println(ci.solution("1234"));
+	}
+
+}
